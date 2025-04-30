27e5f0ccc8625358d248b96c188a453c23b73420
ashrafulatif <ashrafulhaque08@gmail.com>
2025-04-30 23:24:21 +0600

    add: find duplicate parantheses with stack

---
+++ Stack/duplicateParantheses.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class duplicateParantheses {
+    
+     public static boolean isDuplicate(String str){
+         
+         Stack <Character> s= new Stack <>();
+         
+         for (int i =0; i < str.length(); i ++){
+             char ch = str.charAt(i);
+             //for closing 
+             if (ch ==')'){
+                 int count =0;
+                 while (s.pop() != '('){
+                     count++;
+                 }
+                 if (count<1){
+                     return true; //duplicate found
+                 }
+             }
+             else { //for opening
+                 s.push(ch);
+             }
+         }
+         
+         return false;
+     }
+     
+     public static void main(String[] args) {
+         String str="(a+b)";
+         System.out.println(isDuplicate(str));
+     }
+ }
+
