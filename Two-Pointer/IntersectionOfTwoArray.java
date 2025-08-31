02b47c8e5106a2c3df321d6c26f2d565cca2a5d5
ashrafulatif <ashrafulhaque08@gmail.com>
2025-09-01 00:27:58 +0600

    add two pointer approach code

---
+++ Two-Pointer/IntersectionOfTwoArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class IntersectionOfTwoArray {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        //comapre two pointer approach
+        int i=0, j=0;
+        while (i<nums1.length && j< nums2.length){
+            if(nums1[i] == nums2[j]){
+                //check duplicate
+                if(ans.isEmpty() || nums1[i] != ans.get(ans.size()-1)){
+                    ans.add(nums1[i]);
+                }
+                i++;
+                j++;
+            }else if(nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+
+        int[] res = new int[ans.size()];
+        for (int k = 0; k < ans.size(); k++) {
+            res[k] = ans.get(k); 
+        }
+        return res;
+    }
+}
